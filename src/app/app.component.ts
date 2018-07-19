39af2412d07efcdd62fb4711480b026542824bc4
tleveque <thierry.leveque@askida.com>
2018-07-19 14:31:11 -0400

    t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app';
+
+  display: boolean = false;
+
+  content: string = null;
+  docHeader: string = null;
+
+  showDialog() {
+    this.content = null;
+    this.docHeader = 'No doc';
+    this.display = true;
+  }
+
+
+  showDialogLarge() {
+    this.docHeader = 'The doc';
+    this.content = 'Content\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\n\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2\nline 2';
+    this.display = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogModule } from 'primeng/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {
+  AutoCompleteModule,
+  BreadcrumbModule,
+  ConfirmDialogModule,
+  DataTableModule,
+  EditorModule, InputTextModule, OrderListModule, OverlayPanelModule,
+  TooltipModule
+} from 'primeng/primeng';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
@@ -10,7 +24,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DialogModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
